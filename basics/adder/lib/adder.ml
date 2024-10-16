2ebf8493b9ade668da37e4c7bbb67acb1d4d5584
broninx <mbellino004@gmail.com>
2024-10-16 18:54:19 +0200

    Done

--- basics/adder/lib/adder.ml
+++ basics/adder/lib/adder.ml
@@ -1,4 +1,7 @@
 (* val addlist : int list -> int *)
 (* addlist l adds the element of the list of integers l *)
 
-let addlist l = 0 (* replace 0 with actual code *)
+let rec addlist = function
+  | [] -> 0
+  | a :: [] -> a
+  | a :: t -> a + addlist t
--- basics/recognizer/lib/recognizer.ml
+++ basics/recognizer/lib/recognizer.ml
@@ -1,14 +1,31 @@
-let lang1 _ = failwith ""
+let rec lang1  = function
+  | [] -> false
+  | h :: [] -> (h='1' || h = '0')
+  | h:: t -> (h='1' || h = '0') && lang1 t 
 
-let lang2 _ = failwith ""
+let rec lang2  = function 
+  | [] -> true
+  | h::[] when h = '1' || h='0' -> true
+  | h::m::t when h='0' || h='1' -> m='1' && lang2 @@ m::t 
+  | _ -> false
 
-let lang3 _ = failwith ""
+let rec lang3 = function
+  | h :: m :: [] when h='0' && m = '0'-> true  
+  | h:: m  :: t when h = '0'-> (m = '0' || m = '1') && lang3 @@ h::t 
+  | _ -> false
 
-let lang4 _ = failwith ""
+let rec lang4 = function 
+  | h:: m :: [] when h= '1' && m = '1' -> true;
+  | h:: m:: t when not ((List.filter (fun (x) -> not (x='1')) (h::m::t)) = []) ->
+      lang4 @@ List.filter  (fun (x) -> x = '1') (h::m::t) 
+  | _ -> false
 
-let lang5 _ = failwith ""
+let rec lang5 = function
+  | h :: m :: [] when (h = '1' && m = '1') || (h = '0' && m = '0')-> true
+  | h :: m :: t when  (h = '1' && m = '1') || (h = '0' && m = '0')-> lang5 t 
+  | _ -> false
     
 let recognizers = [lang1;lang2;lang3;lang4;lang5]
                   
 let belongsTo w = List.map (fun f -> f w) recognizers
-  
+ 
